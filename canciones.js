321b385f84aabda96101533e58915f7ca06fe552
AlejandraRodriguezz <alejiita902@gmail.com>
2026-09-10 17:38:30 -0500

    Update canciones.js

--- canciones.js
+++ canciones.js
@@ -2,8 +2,9 @@
 const CANCIONES = {
 "2026-09-01": { id: "6MW4akyMVOfnhDbwoYJI4D", nota: "Este fuer nuestro comienzo y realmente siento que nuestro amor es unico"},
   "2026-09-10": { id: "3AJwUDP919kvQ9QcozQPxg", nota: "porque yo quiero amarla en todos los idiomas" },
-  "2026-09-11": { id: "4ou86Vd0SOT7GO4m6V6IAs", nota: "Hola!!!! mi amor hermosa te amo demasiado prometo estar siempre ahi porque solo quiero que sea usted te amo" }
-
+  "2026-09-11": { id: "4ou86Vd0SOT7GO4m6V6IAs", nota: "Hola!!!! mi amor hermosa te amo demasiado prometo estar siempre ahi porque solo quiero que sea usted te amo" },
+"2026-09-06": {id: "0gjhhuqKxswtR1KzYSpSfd", nota: "Realmente espero poder pasar con usted el resto de mi vida, que estemos juntas hoy mañana y hasta que la vida nos permita TE AMO"},
+  "2026-09-04": {id: "1mFF3sXPHmBpto9byINKwA", nota: "Con usted me dan ganas de todo, algo asi como amarla, respetarla, cuidarla como por toda mi vida jajaja"}
   // Copia y pega la línea de abajo para agregar el siguiente día
   // (no olvides la coma al final de la línea anterior):
   // ,"2026-09-11": { id: "TRACK_ID_AQUI", nota: "" }
